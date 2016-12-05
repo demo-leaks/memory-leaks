92638da279ee884779d937dd3f2d3db84e088e67
Artsiom Aliakseichyk <artsiom_aliakseichyk@epam.com>
2016-12-05 17:27:22 +0300

    Added description

--- app/document/documentModule.js
+++ app/document/documentModule.js
@@ -4,7 +4,11 @@ angular.module('documentLeakModule', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/documentLeak', {
-    template: `<h1 ng-init="leakDocument()">Document/Window event listeners memory leak</h1>`,
+    template: `
+    <h1 ng-init="leakDocument()">Document/Window event listeners memory leak</h1>
+    <p>Added event listener to document</p>
+    <p>Start using page (navigate to Home and back). Get heap snapshots. Make sure that in context you will have documentLeakFunction in (scope) section in comparison view</p>
+    `,
     controller: 'documentController'
   });
 }])
